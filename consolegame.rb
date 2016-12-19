d12662514aebf6e873f2b3a9a10183c924c19e08
YsaWV <YsaWV@users.noreply.github.com>
2016-12-18 23:13:53 -0500

    "add player and type of marker"
    "add player and type of marker"

---
+++ consolegame.rb
@@ -0,0 +1,61 @@
+require_relative "tictactoe.rb"
+
+
+class Game
+
+	attr_accessor :board1, :mark, :space
+
+
+	def initialize
+
+		@board1 = Boardtt.new
+
+	end
+
+	def drawboard
+		board1.print_board
+
+	end
+
+	def select_marker
+
+		puts "X or O"
+
+		@mark = gets.chomp.upcase.to_s
+
+		puts """
+		You are playing as #{mark}
+		"""
+
+	end
+
+	def select_position(space)
+		board1.make_mark(@mark,space)
+
+	end
+
+	def repeat_game
+
+		if drawboard == []
+			puts "end game"
+		else select_position(space)
+			#board1.draw_diagonal?
+			puts "win"
+		end
+	
+	end
+
+
+# board1.win_by_diagonal("O")
+# board1.make_mark("O", 0)
+# board1.make_mark("O", 4)
+# board1.make_mark("O", 8)
+
+end
+
+Game1 = Game.new
+Game1.drawboard
+Game1.select_marker
+Game1.select_position(4)
+Game1.repeat_game
+
--- tictactoe.rb
+++ tictactoe.rb
@@ -3,7 +3,8 @@ class Boardtt
 
 	def initialize
 
-	@board = ["_", "_", "_", "_", "_", "_", "_", "_", "_"]
+	#@board = ["_", "_", "_", "_", "_", "_", "_", "_", "_"]
+	@board = Array.new(9," ")
 end
 
 
@@ -11,43 +12,54 @@ def print_board
 	#board = ["_", "_", "_", "_", "_", "_", "_", "_", "_"]
 	# board[0] = 'X'
 	# board[8] = 'X'
-	print @board[0..2]
+	# print @board[0..2]
 	
-	print @board[3..5]
+	# print @board[3..5]
+	# # puts "_*"
+	# print @board[6..9]
 	# puts "_*"
-	print @board[6..9]
-	# puts "_*"
+
+	puts """ 	
+
+			#{board[0]} | #{board[1]} | #{board[2]}
+			_________
+			#{board[3]} | #{board[4]} | #{board[5]}
+			_________
+			#{board[6]} | #{board[7]} | #{board[8]}
+
+	"""
+
+
 
 end
 
-def win_by_diagonal(mark)
+def win_by_diagonal?(mark)
 	#puts board = ["_", "_", "_", "_", "_", "_", "_", "_", "_"]
 	
 
-	puts (board[0] == mark && board[4] == mark && board[8] == mark) || (board[6] = mark && board[4] == mark && board[2] == mark)
+	(board[0] == mark && board[4] == mark && board[8] == mark) || (board[6] = mark && board[4] == mark && board[2] == mark)
 end
 
-def make_mark(mark, space)
-	@board[space] = mark
+def make_mark(mark, space) #update posiiton, mark is either X or O
+	board[space] = mark #space is for the position of the array
 	print_board
 end
 
-# board[0] = 'O'
-# board[4] = 'O'
-# board[8] = 'O'
+
 
 end
 
-board1 = Boardtt.new
+#board1 = Boardtt.new
 # board1.board[0] = "X"
 # board1.board[4] = "X"
 # board1.board[8] = "X"
 
-board1.win_by_diagonal("X")
-board1.make_mark("O", 0)
-board1.make_mark("O", 4)
-board1.make_mark("O", 8)
+#board1.win_by_diagonal("O")
+# board1.make_mark("O", 0)
+#board1.make_mark("O", 4)
+# board1.make_mark("O", 8)
 
+#board1.print_board
 
 
 
